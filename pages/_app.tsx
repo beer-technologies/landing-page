477092e4142d1271da2924a41f17c6b991ac4e99
Olusegun Ekoh <segun.ekoh@gmail.com>
2026-02-11 15:15:44 +0000

    chore(typography): Add Plus Jakara Sans display font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,22 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { Inter } from 'next/font/google';
+import { Inter, Plus_Jakarta_Sans } from 'next/font/google';
 
 const inter = Inter({
   variable: '--font-inter',
   subsets: ['latin'],
 });
 
+const jakarta = Plus_Jakarta_Sans({
+  variable: '--font-display',
+  subsets: ['latin'],
+  weight: ['600', '700', '800'],
+  display: 'swap',
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={`${inter.variable} font-sans`}>
+    <div className={`${inter.variable} ${jakarta.variable} font-sans`}>
       <Component {...pageProps} />
     </div>
   );
